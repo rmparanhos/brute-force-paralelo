a05bcabc41b0ac2a7807cc045f586f8b4e7a3e84
Wallace Baleroni <wallacebaleroni@gmail.com>
2017-07-08 12:01:01 -0300

    Update brute_force_parallel_omp.c

--- brute_force_parallel_omp.c
+++ brute_force_parallel_omp.c
@@ -7,8 +7,12 @@
 #define LIM_INF_GLOBAL '!'
 #define LIM_SUP_GLOBAL '~'
 
+#define NAO_ENCONTRADA -1
+#define DESNECESSARIA -2
+
 
 char *objetivo;
+int encontrada;
 
 // Função que valida se a cadeia é a senha buscada
 int validacao(char cadeia[]) {
@@ -17,7 +21,7 @@ int validacao(char cadeia[]) {
 
 // Função que varia a string do inicio ao fim
 double varia(char **senha, char lim_inf[], char lim_sup[]) {
-	double iteracao = -1;
+	double iteracao = 0;
 	
 	// Descobre tamanho	
 	size_t tamanho = strlen(lim_inf);
@@ -28,6 +32,15 @@ double varia(char **senha, char lim_inf[], char lim_sup[]) {
 
 	// Loop da variação
 	while(strcmp(atual, lim_sup) != 0) {
+		// Valida senha
+		if(validacao(atual)) {
+			// Aloca e retorna
+			*senha = (char *) malloc(strlen(lim_inf) + 1);
+			(*senha)[strlen(lim_inf)] = '\0';
+			strcpy(*senha, atual);
+			return iteracao;
+		}
+
 		// Incrementa o final da string
 		atual[tamanho - 1]++;
 		int i;
@@ -44,19 +57,20 @@ double varia(char **senha, char lim_inf[], char lim_sup[]) {
 				continue;
 			}
 		}
-		// Valida senha
-		if(validacao(atual)) {
-			// Aloca e retorna
-			*senha = (char *) malloc(strlen(lim_inf) + 1);
-			(*senha)[strlen(lim_inf)] = '\0';
-			strcpy(*senha, atual);
-			return iteracao;
-		}
 
 		iteracao++;
+		
+		if(encontrada) return DESNECESSARIA;
+	}
+	if(validacao(atual)) {
+		// Aloca e retorna
+		*senha = (char *) malloc(strlen(lim_inf) + 1);
+		(*senha)[strlen(lim_inf)] = '\0';
+		strcpy(*senha, atual);
+		return iteracao;
 	}
 
-	return -1;
+	return NAO_ENCONTRADA;
 }
 
 int somatorio(int n_processos, int distribuicao[n_processos]) {
@@ -92,34 +106,16 @@ int *distribui(int n_processos) {
 }
 
 int main() {
+	encontrada = 0;
+	
 	objetivo = (char *) malloc(sizeof(char) * 155);
 	printf("Insira a senha objetivo: ");
 	scanf("%s", objetivo);
-	int n;
-	printf("\nInsira o numero de threads: ");
-	scanf("%d", &n);
-
-	/*char lim_inf[strlen(objetivo) + 1], lim_sup[strlen(objetivo) + 1];
-	int i;
-	for(i = 0; i < strlen(objetivo); i++) {
-		lim_inf[i] = LIM_INF;
-		lim_sup[i] = LIM_SUP;
-	}
-	lim_inf[strlen(objetivo)] = '\0';
-	lim_sup[strlen(objetivo)] = '\0';*/
-
-	/*
-	char *senha = NULL;
-	int n = 4;
-	char lims_inf[n][strlen(objetivo)];
-	char lims_sup[n][strlen(objetivo)];
-	for(i=0;i<n;i++){
-		if(i==0){
-			strcpy(lims_inf[i],lim_inf);
-		}	
-	}
-	*/
 	
+	int n_threads;
+	printf("Insira o numero de threads: ");
+	scanf("%d", &n_threads);
+
 	int i;
 	int tamanho_intervalo = LIM_SUP_GLOBAL - LIM_INF_GLOBAL + 1;
 	char *intervalo = (char *) malloc(sizeof(char) * tamanho_intervalo);
@@ -127,75 +123,68 @@ int main() {
 		intervalo[i] = i + LIM_INF_GLOBAL;
 	}
 
-	// Cria o array de distribuição do Scatterv
-	int *distribuicao = (int *) malloc(sizeof(int) * n);
-	distribuicao = distribui(n);
+	// Cria o array de distribuição
+	int *distribuicao = (int *) malloc(sizeof(int) * n_threads);
+	distribuicao = distribui(n_threads);
 
-	// Cria o array de displs(offsets) pro Scatterv
-	int displs[n];
-	int offset = 0;
-	for(i = 0; i < n; i++) {
-		displs[i] = offset;
-		offset += distribuicao[i];
-
-	}
+	// Cria o array de posicoes de lim_sup
 	char *senha = NULL; 
-	int posicoes[n];
-	int soma;
-	for(i=0;i<n;i++){
-		if(i==0) posicoes[i]=0;
-		else posicoes[i] = soma;
-		soma += distribuicao[i]	;
+	int posicoes[n_threads];
+	int soma = 0;
+	for(i = 0; i < n_threads; i++){
+		if(i == 0) {
+			posicoes[i] = LIM_INF_GLOBAL;
+			soma = LIM_INF_GLOBAL;
+		} else {
+			soma += distribuicao[i];
+			posicoes[i] = soma;
+		}
 	}
 
-	#pragma omp parallel num_threads(n)
+	#pragma omp parallel num_threads(n_threads)
 	{
-	int id = omp_get_thread_num();
-	char *limites = malloc(sizeof(char) * distribuicao[id]);
-	
-	if(id==0){
-		printf("33  ");
-		printf("%c\n",intervalo[0]);
-	}
-	else{
-		printf("%d  ",posicoes[id]+33);
-		printf("%c\n",intervalo[posicoes[id]]);	
-	}
-	
-	//limites = posicoes[id];
-	
-	char lim_inf = posicoes[id];
-	char lim_sup;
-	if(id == n-1){
-		lim_sup = LIM_SUP_GLOBAL;
-	}
-	else{
-		lim_sup = posicoes[id+1]-1;
-	}
-	// Constroi as strings de limite e envia pra função
-	char str_lim_inf[strlen(objetivo) + 1], str_lim_sup[strlen(objetivo) + 1];
-	str_lim_inf[0] = lim_inf;
-	str_lim_sup[0] = lim_sup;
-	for(i = 1; i < strlen(objetivo); i++) {
-		str_lim_inf[i] = LIM_INF_GLOBAL;
-		str_lim_sup[i] = LIM_SUP_GLOBAL;
-	}
-	str_lim_inf[strlen(objetivo)] = '\0';
-	str_lim_sup[strlen(objetivo)] = '\0';
-	clock_t t_inicio, t_fim;
-    	double t_total;
-     
-    	t_inicio = clock();
-	
-	double iteracao = varia(&senha, str_lim_inf, str_lim_sup); 
-	t_fim = clock();
-    	t_total = ((double) (t_fim - t_inicio)) / CLOCKS_PER_SEC;
+		int id = omp_get_thread_num();
+		char *limites = malloc(sizeof(char) * distribuicao[id]);
+		
+		char lim_inf = posicoes[id];
+		char lim_sup;
+		if(id == n_threads - 1) {
+			lim_sup = LIM_SUP_GLOBAL;
+		} else {
+			lim_sup = posicoes[id + 1] - 1;
+		}
 
-	if(iteracao != -1) {
-		printf("Senha \"%s\" descoberta na iteração %.f em %f segundos\n", senha, iteracao, t_total);
-	} else {
+		// Constroi as strings de limite e envia pra função
+		char str_lim_inf[strlen(objetivo) + 1], str_lim_sup[strlen(objetivo) + 1];
+		str_lim_inf[0] = lim_inf;
+		str_lim_sup[0] = lim_sup;
+
+		for(i = 1; i < strlen(objetivo); i++) {
+			str_lim_inf[i] = LIM_INF_GLOBAL;
+			str_lim_sup[i] = LIM_SUP_GLOBAL;
+		}
+		str_lim_inf[strlen(objetivo)] = '\0';
+		str_lim_sup[strlen(objetivo)] = '\0';
+		clock_t t_inicio, t_fim;
+	    double t_total;
+
+	    printf("thread: %d\nlim_inf: %s\nlim_sup: %s\n", id, str_lim_inf, str_lim_sup);
+	     
+	    t_inicio = clock();
+
+		double iteracao = varia(&senha, str_lim_inf, str_lim_sup); 
+		
+		t_fim = clock();
+	    t_total = ((double) (t_fim - t_inicio)) / CLOCKS_PER_SEC;
+
+		if(iteracao != DESNECESSARIA) {
+			if(iteracao != NAO_ENCONTRADA) {
+	    		encontrada = 1;
+				printf("Senha \"%s\" descoberta na iteração %.f em %f segundos\n", senha, iteracao, t_total);
+			}
+		}
+	}
+	if(!encontrada) {
 		printf("Senha não encontrada\n");
 	}
-	}
-
 }
